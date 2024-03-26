f5d6961b766888ddde673f8d65b4d5acb374b23f
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-03-25 23:30:31 -0400

    Add midsemester self reflection

--- main/grade_calculator.py
+++ main/grade_calculator.py
@@ -1,20 +1,31 @@
 from main.criterion import Criterion
+from main.mapped_criterion import MappedCriterion
 
 class GradeCalculator:
     GRADES = ["A", "B", "C", "D", "F"]
     ASSIGNMENT_MINIMUMS = [7, 6, 5, 4]
     ACHIEVEMENT_MINIMUMS = [5, 4, 3, 2]
+    MIDSEMESTER_MINIMUMS = [1]
 
-    def __init__(self, assignments: int, achievements: int):
-        self.assignments = assignments
-        self.achievements = achievements
+    MIDSEMESTER_MAPPING = [0, 3]
+
+
+    def __init__(self, assignments: int, achievements: int, midsemester: int):
+        self.criteria_inputs = [
+            assignments,
+            achievements,
+            midsemester,
+        ]
+        self.criteria = [
+            Criterion(self.ASSIGNMENT_MINIMUMS),
+            Criterion(self.ACHIEVEMENT_MINIMUMS),
+            MappedCriterion(self.MIDSEMESTER_MINIMUMS, self.MIDSEMESTER_MAPPING),
+        ]
 
     def calculateGrade(self) -> str:
-        assignment_criterion = Criterion(self.ASSIGNMENT_MINIMUMS)
-        assignment_level = assignment_criterion.get_level(self.assignments)
+        max_grade_level = 0
+        for criterion_input, criterion in zip(self.criteria_inputs, self.criteria):
+            grade_level = criterion.get_level(criterion_input)
+            max_grade_level = max(max_grade_level, grade_level)
 
-        achievement_criterion = Criterion(self.ACHIEVEMENT_MINIMUMS)
-        achievement_level = achievement_criterion.get_level(self.achievements)
-
-        final_grade_level = max(assignment_level, achievement_level)
-        return self.GRADES[final_grade_level]
+        return self.GRADES[max_grade_level]
--- test/grade_calculator_test.py
+++ test/grade_calculator_test.py
@@ -13,7 +13,7 @@ ASSIGNMENT_TEST_DATA = [
 
 @pytest.mark.parametrize("count,grade", ASSIGNMENT_TEST_DATA)
 def test_assignments(count: int, grade: str) -> None:
-    calculator = GradeCalculator(count, 5)
+    calculator = GradeCalculator(count, 5, 1)
     result_grade = calculator.calculateGrade()
     assert result_grade == grade
 
@@ -29,7 +29,19 @@ ACHIEVEMENT_TEST_DATA = [
 
 @pytest.mark.parametrize("count,grade", ACHIEVEMENT_TEST_DATA)
 def test_achievements(count: int, grade: str) -> None:
-    calculator = GradeCalculator(7, count)
+    calculator = GradeCalculator(7, count, 1)
     result_grade = calculator.calculateGrade()
     assert result_grade == grade
 
+
+MIDSEMESTER_TEST_DATA = [
+    (1, "A"),
+    (0, "D"),
+]
+
+
+@pytest.mark.parametrize("completed,grade", MIDSEMESTER_TEST_DATA)
+def test_midsemester(completed: int, grade: str) -> None:
+    calculator = GradeCalculator(7, 5, completed)
+    result_grade = calculator.calculateGrade()
+    assert result_grade == grade
