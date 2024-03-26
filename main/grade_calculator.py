470240d31617850c48e4d6a2e15198621702e306
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-03-25 22:04:06 -0400

    Implemented achievements

--- main/grade_calculator.py
+++ main/grade_calculator.py
@@ -3,11 +3,18 @@ from main.criterion import Criterion
 class GradeCalculator:
     GRADES = ["A", "B", "C", "D", "F"]
     ASSIGNMENT_MINIMUMS = [7, 6, 5, 4]
+    ACHIEVEMENT_MINIMUMS = [5, 4, 3, 2]
 
-    def __init__(self, assignments: int):
+    def __init__(self, assignments: int, achievements: int):
         self.assignments = assignments
+        self.achievements = achievements
 
     def calculateGrade(self) -> str:
         assignment_criterion = Criterion(self.ASSIGNMENT_MINIMUMS)
         assignment_level = assignment_criterion.get_level(self.assignments)
-        return self.GRADES[assignment_level]
+
+        achievement_criterion = Criterion(self.ACHIEVEMENT_MINIMUMS)
+        achievement_level = achievement_criterion.get_level(self.achievements)
+
+        final_grade_level = max(assignment_level, achievement_level)
+        return self.GRADES[final_grade_level]
--- test/grade_calculator_test.py
+++ test/grade_calculator_test.py
@@ -11,6 +11,21 @@ ASSIGNMENT_TEST_DATA = [(7, "A"),
 
 @pytest.mark.parametrize("count,grade", ASSIGNMENT_TEST_DATA)
 def test_assignments(count: int, grade: str) -> None:
-    calculator = GradeCalculator(count)
+    calculator = GradeCalculator(count, 5)
     result_grade = calculator.calculateGrade()
     assert result_grade == grade
+
+
+ACHIEVEMENT_TEST_DATA = [(5, "A"),
+                        (4, "B"),
+                        (3, "C"),
+                        (2, "D"),
+                        (1, "F")]
+
+
+@pytest.mark.parametrize("count,grade", ACHIEVEMENT_TEST_DATA)
+def test_achievements(count: int, grade: str) -> None:
+    calculator = GradeCalculator(7, count)
+    result_grade = calculator.calculateGrade()
+    assert result_grade == grade
+
